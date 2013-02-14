befa0df9ff08552b19d1de8b735808ba6b75f575
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-02-14 19:29:49 +0000

    refactored test

--- test/scratch/ScratchConfigTest.java
+++ test/scratch/ScratchConfigTest.java
@@ -14,14 +14,21 @@ import static scratch.ScratchConfig.UP;
  * Date: 14/02/2013
  */
 public class ScratchConfigTest {
-	@Test public void movingScratchesInConfig() {
-		Scratch scratch1 = Scratch.createFrom("scratch1.txt");
-		Scratch scratch2 = Scratch.createFrom("scratch2.txt");
-		Scratch scratch3 = Scratch.createFrom("scratch3.txt");
-		ScratchConfig config = DEFAULT_CONFIG.with(list(scratch1, scratch2, scratch3));
 
+	private final Scratch scratch1 = Scratch.createFrom("scratch1.txt");
+	private final Scratch scratch2 = Scratch.createFrom("scratch2.txt");
+	private final Scratch scratch3 = Scratch.createFrom("scratch3.txt");
+	private final ScratchConfig config = DEFAULT_CONFIG.with(list(scratch1, scratch2, scratch3));
+
+	@Test public void movingTopScratchToBottom() {
 		assertThat(config.move(scratch1, UP), equalTo(DEFAULT_CONFIG.with(list(scratch2, scratch3, scratch1))));
-		assertThat(config.move(scratch2, UP), equalTo(DEFAULT_CONFIG.with(list(scratch2, scratch1, scratch3))));
+	}
+
+	@Test public void movingBottomScratchToTop() {
 		assertThat(config.move(scratch3, DOWN), equalTo(DEFAULT_CONFIG.with(list(scratch3, scratch1, scratch2))));
+	}
+
+	@Test public void movingScratchUp() {
+		assertThat(config.move(scratch2, UP), equalTo(DEFAULT_CONFIG.with(list(scratch2, scratch1, scratch3))));
 	}
 }
