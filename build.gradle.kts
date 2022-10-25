96c16ca49300c528117eb5a875e6f1702178839a
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2022-10-25 11:14:35 +0100

    since-build="222.3345.118"; kotlin 1.6.21; jvm 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 import org.gradle.api.internal.HasConvention
 import org.jetbrains.intellij.IntelliJPluginExtension
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
     idea
     java
-    kotlin("jvm").version("1.3.72")
-    id("org.jetbrains.intellij").version("0.4.18")
+    kotlin("jvm").version("1.6.21")
+    id("org.jetbrains.intellij").version("1.9.0")
 }
 repositories {
     mavenCentral()
@@ -24,14 +24,14 @@ sourceSets {
 }
 
 dependencies {
-    testImplementation("org.mockito:mockito-inline:3.3.3")
+    testImplementation("org.mockito:mockito-inline:4.8.0")
 }
 
 tasks.withType<KotlinJvmCompile> {
     kotlinOptions {
-        jvmTarget = "11"
-        apiVersion = "1.3"
-        languageVersion = "1.3"
+        jvmTarget = "17"
+        apiVersion = "1.6"
+        languageVersion = "1.6"
         // Compiler flag to allow building against pre-released versions of Kotlin
         // because IJ EAP can be built using pre-released Kotlin but it's still worth doing to check API compatibility
         freeCompilerArgs = freeCompilerArgs + listOf("-Xskip-metadata-version-check")
@@ -41,15 +41,15 @@ tasks.withType<KotlinJvmCompile> {
 configure<IntelliJPluginExtension> {
     // See https://www.jetbrains.com/intellij-repository/releases for a list of available IDEA builds
     val ideVersion = System.getenv().getOrDefault("IJ_VERSION",
-//            "IC-193.5233.102"
-        "LATEST-EAP-SNAPSHOT"
+        "222.3345.118"
+//        "LATEST-EAP-SNAPSHOT"
     )
     println("Using ide version: $ideVersion")
-    version = ideVersion
-    pluginName = "Scratch"
-    downloadSources = true
-    sameSinceUntilBuild = false
-    updateSinceUntilBuild = false
+    version.set(ideVersion)
+    pluginName.set("Scratch")
+    downloadSources.set(true)
+    sameSinceUntilBuild.set(false)
+    updateSinceUntilBuild.set(false)
 }
 
 val SourceSet.kotlin: SourceDirectorySet
